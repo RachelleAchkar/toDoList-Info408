e84eb680db19e8f154933213306860d556f3812c
Rachelle <rachelleachkar.ra@gmail.com>
2025-05-22 17:11:03 +0300

    prettier corrections

--- src/components/__tests__/AddTask.test.jsx
+++ src/components/__tests__/AddTask.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import {TodoForm} from '../TodoForm';
+import { TodoForm } from '../TodoForm';
 import '@testing-library/jest-dom';
 
 test('renders input field and add button', () => {
